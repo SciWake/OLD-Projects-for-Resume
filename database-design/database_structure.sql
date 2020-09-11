c096788afdb536035547409da9961a497635612c
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-11 18:15:45 +0700

    Created a table of groups and necessary links

--- database-design/database_structure.sql
+++ database-design/database_structure.sql
@@ -22,7 +22,7 @@ CREATE TABLE `profiles` (
   `birthday` DATE NOT NULL COMMENT 'Дата рождения',
   `country` VARCHAR(255) NOT NULL COMMENT 'Страна проживания',
   `photo_id` INT UNSIGNED,
-  `user_description` VARCHAR(255) COMMENT 'Описание( О себе )',
+  `user_description` VARCHAR(255) COMMENT 'Описание пользователя',
   `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Время создания строки',  
   `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Время обновления строки'
 ) COMMENT 'Профиль пользователя';
@@ -41,3 +41,36 @@ CREATE TABLE `contacts_statuses` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(128) NOT NULL UNIQUE
 ) COMMENT 'Тип статуса';
+
+
+-- Таблица групп
+CREATE TABLE `groups` (
+  `id` SERIAL PRIMARY KEY,
+  `name` VARCHAR(128) NOT NULL,
+  `link` VARCHAR(128) NOT NULL UNIQUE,
+  `photo_id` INT UNSIGNED,
+  `description` VARCHAR(255),
+  `is_open` BOOLEAN NOT NULL COMMENT 'Тип канала {0 : Закрытый, 1 : Открытый}',
+  `chat_history` BOOLEAN NOT NULL COMMENT 'Видна ли история чата',
+  `user_permissions_id` INT UNSIGNED
+) COMMENT 'Группа';
+
+-- Таблица устанавливает разрешения пользователей
+CREATE TABLE `user_permissions_id` (
+  `group_id` SERIAL PRIMARY KEY,
+  `can_send_massages` BOOLEAN,
+  `can_send_media` BOOLEAN,
+  `can_send_sticers_gif` BOOLEAN,
+  `can_preciew_links` BOOLEAN,
+  `can_creating_posts` BOOLEAN,
+  `can_creating_surveys` BOOLEAN,
+  `can_adding_person` BOOLEAN
+);
+
+-- Таблица связи пользователей и групп
+CREATE TABLE `groups_membership` (
+  `group_id` INT UNSIGNED NOT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
+  `user_type_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`group_id`, `user_id`)
+);
