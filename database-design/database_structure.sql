19c276a214f838360b35323141a882012909a7bc
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-11 13:54:35 +0700

    Profile table created

--- database-design/database_structure.sql
+++ database-design/database_structure.sql
@@ -11,3 +11,19 @@ CREATE TABLE `users` (
   `created_at` DATETIME DEFAULT NOW() COMMENT 'Время создания строки',
   `updated_at` DATETIME DEFAULT NOW() ON UPDATE NOW() COMMENT 'Время обновления строки'
 ) COMMENT 'Пользователи';
+
+
+-- Таблица профилей
+CREATE TABLE `profiles` (
+  `user_id` SERIAL PRIMARY KEY,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255),
+  `nick_name` VARCHAR(32) UNIQUE,
+  `gender` BOOLEAN NOT NULL COMMENT '{0 : Мужской, 1 : Женский}',
+  `birthday` DATE NOT NULL COMMENT 'Дата рождения',
+  `country` VARCHAR(255) NOT NULL COMMENT 'Страна проживания',
+  `photo_id` INT UNSIGNED,
+  `user_description` VARCHAR(255) COMMENT 'Описание( О себе )',
+  `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Время создания строки',  
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Время обновления строки'
+);
