6fca1ff4eb741c9029677d86555ae9cc664569ff
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-27 21:19:32 +0700

    Updating the procedure
    Added an error handler to the procedure to avoid terminating it due to data insertion errors.

--- database-design/procedure_messages_users.sql
+++ database-design/procedure_messages_users.sql
@@ -1,4 +1,10 @@
--- Обновление данных в таблице messages_users
+-- Выбор базы данных
+USE telegram;
+
+
+
+-- ___________________________________________________________
+-- ЗАПРОСЫ, КОТОРЫЕ БУДУТ ИСПОЛЬЗОВАНЫ В ПРОЦЕДУРЕ
 
 -- Выбираем сообщения, которые отправлены в группу
 SELECT * 
@@ -13,7 +19,7 @@ SELECT COUNT(*)
   FROM messages_users 
     WHERE target_type_id IN (SELECT id FROM target_types WHERE name = 'groups')
       AND target_id NOT IN (SELECT id FROM `groups`);
--- 1615
+-- 243
 
 -- Вариант с JOIN
 SELECT COUNT(*)
@@ -21,7 +27,8 @@ SELECT COUNT(*)
       INNER JOIN target_types
         ON messages_users.target_type_id = target_types.id AND target_types.name = 'groups'
 	WHERE target_id NOT IN (SELECT id FROM `groups`);
--- 1615
+-- 243
+
 
 -- Выполняем обновление данных
 
@@ -30,6 +37,7 @@ UPDATE messages_users
   SET target_id = (SELECT id FROM `groups` ORDER BY RAND() LIMIT 1)
   WHERE target_type_id IN (SELECT id FROM target_types WHERE name = 'groups')
     AND target_id NOT IN (SELECT id FROM `groups`);
+    
 /* Error Code: 1062. Duplicate entry '4-17-2' for key 'messages_users.PRIMARY' 
 Чтобы изавиться от данной ошибки, требуется установить лимит обновления записей LIMIT 1, тогда обновление будет успешным.
 
@@ -39,7 +47,7 @@ UPDATE messages_users
 
 DELIMITER //
 
-/*Функция возвращает TRUE, если есть данные для обновления, иначе FALSE*/
+-- Функция возвращает TRUE, если есть данные для обновления, иначе FALSE
 DROP FUNCTION IF EXISTS update_groups//
 CREATE FUNCTION update_groups()
 RETURNS INT DETERMINISTIC
@@ -76,23 +84,32 @@ DROP PROCEDURE IF EXISTS update_messages_users_in_group//
 CREATE PROCEDURE update_messages_users_in_group()
 BEGIN
 
-  WHILE update_groups() DO
+  DECLARE CONTINUE HANDLER FOR SQLSTATE '23000' SET @error = 'Ошибка вставки значения';
+  
+  cycle: WHILE update_groups() DO
 	UPDATE messages_users
 	  SET target_id = (SELECT id FROM `groups` ORDER BY RAND() LIMIT 1)
       WHERE target_type_id IN (SELECT id FROM target_types WHERE name = 'groups')
-        AND target_id NOT IN (SELECT id FROM `groups`) LIMIT 1;
+        AND target_id NOT IN (SELECT id FROM `groups`) LIMIT 15;
+        
+	IF @error IS NOT NULL THEN ITERATE cycle;
+    END IF;
+    
   END WHILE;
   
 END//
+
 
 DELIMITER ;
 
 -- Запускаем процедуру
 CALL update_messages_users_in_group;
 
+
 -- С помощью функции проверяем, есть ли данные для обновления
 SELECT update_groups();
 -- 0
+
 
 -- Анализ итоговых данных
 
