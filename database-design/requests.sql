61f10d75c9034ba3f6e8d4da7e57a9fd1cea07f7
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-29 08:24:37 +0700

    Selecting a specific user's correspondence

--- database-design/requests.sql
+++ database-design/requests.sql
@@ -20,3 +20,23 @@ SELECT posts.community_id,
 GROUP BY posts.community_id
 ORDER BY count_views_groups DESC
 LIMIT 3;
+
+
+-- Сообщения от пользователя
+SELECT messages.user_id, messages.body, messages_users.target_id
+    FROM messages
+      INNER JOIN messages_users
+        ON messages.id = messages_users.messages_id
+	  INNER JOIN target_types
+        ON messages_users.target_type_id = target_types.id AND target_types.name = 'users'
+	WHERE messages.user_id = 65;
+
+-- Сообщение от пользователя и к пользователю (Переписка пользователя 65)
+SELECT messages.user_id, messages.body, messages_users.target_id
+    FROM messages
+      INNER JOIN messages_users
+        ON messages.id = messages_users.messages_id 
+	  INNER JOIN target_types
+        ON messages_users.target_type_id = target_types.id 
+          AND target_types.name = 'users'
+	WHERE messages_users.target_id = 65 OR messages.user_id = 65;
