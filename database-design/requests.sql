9c99f3cb50a681783c49641dfbc1445f8b87444e
Aleksey Kuritsyn <finloukuritsun2090@yandex.com>
2020-09-29 16:45:39 +0700

    Added window function
    The window function displays the name of the group, the average number of users in groups, the youngest in the group, the oldest in the group, the total number of users in the group, all users in the system, the ratio in percent (the total number of users in the group / users in the system) * 100.

--- database-design/requests.sql
+++ database-design/requests.sql
@@ -40,3 +40,34 @@ SELECT messages.user_id, messages.body, messages_users.target_id
         ON messages_users.target_type_id = target_types.id 
           AND target_types.name = 'users'
 	WHERE messages_users.target_id = 65 OR messages.user_id = 65;
+
+
+-- Построить запрос, который будет выводить следующие столбцы:
+-- имя группы
+-- среднее количество пользователей в группах
+-- самый молодой пользователь в группе
+-- самый старший пользователь в группе
+-- общее количество пользователей в группе
+-- всего пользователей в системе
+-- отношение в процентах 
+-- (общее количество пользователей в группе /  всего пользователей в системе) * 100
+
+SELECT DISTINCT 
+  communities.name AS canal_name,
+  COUNT(communities_users.user_id) OVER() 
+    / (SELECT COUNT(*) FROM communities) AS avg_users_in_groups,
+  FIRST_VALUE(CONCAT_WS(" ", profiles.first_name, profiles.last_name)) OVER w_community_birthday_desc AS youngest,
+  FIRST_VALUE(CONCAT_WS(" ", profiles.first_name, profiles.last_name)) OVER w_community_birthday_asc AS oldest,
+  COUNT(communities_users.user_id) OVER w_community AS users_in_canal,
+  (SELECT COUNT(*) FROM users) AS users_total,
+  COUNT(communities_users.user_id) OVER w_community / (SELECT COUNT(*) FROM users) * 100 AS '%%'
+    FROM communities
+      LEFT JOIN communities_users 
+        ON communities_users.community_id = communities.id
+      LEFT JOIN users 
+        ON communities_users.user_id = users.id
+      LEFT JOIN profiles 
+        ON profiles.user_id = users.id
+      WINDOW w_community AS (PARTITION BY communities.id),
+             w_community_birthday_desc AS (PARTITION BY communities.id ORDER BY profiles.birthday DESC),
+             w_community_birthday_asc AS (PARTITION BY communities.id ORDER BY profiles.birthday);
