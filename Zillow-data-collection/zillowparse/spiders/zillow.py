0cd8e54609088d94a1295d676dfadf7151f26450
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-06 04:41:50 +0700

    Implementing getting pagination links

--- Zillow-data-collection/zillowparse/spiders/zillow.py
+++ Zillow-data-collection/zillowparse/spiders/zillow.py
@@ -1,10 +1,11 @@
 import scrapy
 
-
 class ZillowSpider(scrapy.Spider):
     name = 'zillow'
     allowed_domains = ['www.zillow.com']
-    start_urls = ['http://www.zillow.com/']
+    start_urls = ['https://www.zillow.com/homes/Washington_rb/']
 
     def parse(self, response):
-        pass
+        # Getting pagination links
+        for pag_url in response.xpath('//nav[@aria-label="Pagination"]//a[@title="Next page"]/@href'):
+            yield response.follow(pag_url, callback=self.parse)
