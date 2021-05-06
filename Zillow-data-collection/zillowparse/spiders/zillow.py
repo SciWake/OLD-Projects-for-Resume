b8174a2c5843d2242c3fae665f7e0f763cdc4f0c
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-06 08:37:31 +0700

    A function for cleaning JSON data has been written

--- Zillow-data-collection/zillowparse/spiders/zillow.py
+++ Zillow-data-collection/zillowparse/spiders/zillow.py
@@ -1,4 +1,8 @@
 import scrapy
+import json
+from scrapy.loader import ItemLoader
+from zillowparse.items import ZillowparseItem
+import time
 
 
 class ZillowSpider(scrapy.Spider):
@@ -12,6 +16,26 @@ class ZillowSpider(scrapy.Spider):
             yield response.follow(pag_url, callback=self.parse)
 
         # Getting links to apartments
-        for ads_url in response.xpath(
+        for house_url in response.xpath(
                 "//ul[contains(@class, 'photo-cards_short')]/li/article/div[@class='list-card-info']/a/@href"):
-            yield response.follow(ads_url, callback=self.ads_parse)
+            yield response.follow(house_url, callback=self.house_parse)
+
+    @staticmethod
+    def clear_data(item, data):
+        data_json = json.loads(data)['apiCache']
+        # We cut the first part of json to the right moment
+        data_json = data_json[data_json.index('ForSaleDoubleScrollFullRenderQuery'):]
+
+        index_end = data_json.index('{"property"')  # Cut off the beginning of the key to replace it with a new one
+        data_json = '{"info":' + data_json[index_end:]  # Since we cut the first key, we will create a new one
+
+        return json.loads(data_json)
+
+
+    def house_parse(self, response):
+        item = ItemLoader(ZillowparseItem(), response)
+        t0 = time.process_time()
+        data = self.clear_data(item, response.css("script#hdpApolloPreloadedData::text").extract()[0])
+        t1 = time.process_time()
+        # 0.078125
+        print(1)
