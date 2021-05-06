3646dff90076232be0da8bbf6e7f661f796a65ea
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-06 08:38:01 +0700

    Basic Items created

--- Zillow-data-collection/zillowparse/items.py
+++ Zillow-data-collection/zillowparse/items.py
@@ -4,9 +4,11 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
 
 
 class ZillowparseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    id = scrapy.Field(input_processor=MapCompose(lambda data: int(data[2:])), output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
+    photos = scrapy.Field()
