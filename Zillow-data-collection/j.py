aae25446d41bc7fe69df17772a54a95ddf11b41c
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-06 17:12:23 +0700

    Drop file

--- Zillow-data-collection/j.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-import time
-
-driver = webdriver.Chrome()
-driver.get('https://geekbrains.ru/login')
